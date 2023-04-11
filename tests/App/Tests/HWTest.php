e41ad566234810f461a2fafd81b4ae65c49090eb
zeroicq <zeroicq@gmail.com>
2023-04-11 13:04:07 +1000

    Add PHPunit

---
+++ tests/App/Tests/HWTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests;
+use PHPUnit\Framework\TestCase;
+
+class HWTest extends TestCase
+{
+    public function testName(): void
+    {
+        $a = 'name';
+        $this->assertSame('name', $a);
+        $this->markTestIncomplete();
+    }
+}
